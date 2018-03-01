e363e2ce0bc4a91c4f807d9763ed15388c0db54c
vm10ferrera <vm10ferrera@gmail.com>
2018-03-01 20:56:45 -0300

    Exercicios

--- src/Aula/Aula.java
+++ src/Aula/Aula.java
@@ -3,6 +3,20 @@ package Aula;
 public class Aula {
 	public static void main(String args []){
 	System.out.println("Teste testado");
+	//Exercio 07
+		int r = 12;
+	    float pi = (float) 3.1415;
+	    double area = pi*Math.pow(r,2); 
+	    System.out.println("A área é: " + area);
+	    
+	    //Exercicio 08
+	    int resto = 99 % 4;
+	    System.out.println("O resto é: " + resto);
+	    
+	    //Exercicio 09
+	    int numero = 7;
+	    double resultado = numero*0.5;
+	    System.out.println("O resultado é: " + resultado);
 
 }
 }
---
+++ src/Aula/Exercicios.java
@@ -0,0 +1,25 @@
+package Aula;
+
+public class Exercicios {
+public static void main(String args []){
+	
+	//Exercio 07
+	int r = 12;
+    float pi = (float) 3.1415;
+    double area = pi*Math.pow(r,2); 
+    System.out.println("A área é: " + area);
+    
+    //Exercicio 08
+    int resto = 99 % 4;
+    System.out.println("O resto é: " + resto);
+    
+    //Exercicio 09
+    int numero = 7;
+    double resultado = numero*0.5;
+    System.out.println("O resultado é: " + resultado);
+    
+
+   
+    
+}
+}
