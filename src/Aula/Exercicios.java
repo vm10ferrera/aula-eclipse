925bfeda9f0a24873e1b9cb795487ce7d924e3a9
vm10ferrera <36748777+vm10ferrera@users.noreply.github.com>
2018-03-06 17:39:16 -0300

    Update Exercicios.java

--- src/Aula/Exercicios.java
+++ src/Aula/Exercicios.java
@@ -7,17 +7,21 @@ public static void main(String args []){
 	int r = 12;
     float pi = (float) 3.1415;
     double area = pi*Math.pow(r,2); 
-    System.out.println("A área é: " + area);
+    System.out.println("A Ã¡rea Ã©: " + area);
     
     //Exercicio 08
     int resto = 99 % 4;
-    System.out.println("O resto é: " + resto);
+    System.out.println("O resto Ã©: " + resto);
     
     //Exercicio 09
     int numero = 7;
     double resultado = numero*0.5;
-    System.out.println("O resultado é: " + resultado);
-    
+    System.out.println("O resultado Ã©: " + resultado);
+	
+    //Exercicio 10
+	    int numero1 = 1;
+	    double Multi = numero1 + numero1 + numero1 + numero1 + numero1 + numero1 + numero1 + numero1;
+	    System.out.println("O resultado Ã©: " + Multi);    
 
    
     
