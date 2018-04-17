8f0cf73d6e047edb6f74210e19b798a15dbf70d6
Vinícius Ferreira <36748777+vm10ferrera@users.noreply.github.com>
2018-04-16 23:52:23 -0300

    Update Calculos.java

--- Calculadora/src/calculos/Calculos.java
+++ Calculadora/src/calculos/Calculos.java
@@ -2,63 +2,63 @@ package calculos;
 
 public class Calculos {
 	
-	public double mAdi(double n0, double n1) {
-    	double resultado = n0 + n1;
+	public double mAdi(double n1, double n2) {
+    	double resultado = n1 + n2;
     	return resultado;
     }
 	
-	public double mSub(double n0, double n1) {
-    	double resultado = n0 - n1;
+	public double mSub(double n1, double n2) {
+    	double resultado = n1 - n2;
     	return resultado;
     }
 	
-	public double rQ(double n0) {
-    	double resultado = java.lang.Math.sqrt(n0);
+	public double rQ(double n1) {
+    	double resultado = java.lang.Math.sqrt(n1);
     	return resultado;
     }
 	
-	public double divi(double n0, double n1) {
-    	double resultado = n0 / n1;
+	public double divi(double n1, double n2) {
+    	double resultado = n1 / n2;
     	return resultado;
     }
 	
-	public double diviPorcent(double n0, double n1) {
-    	double resultado = n0 / ((n0 * n1) / 100);
+	public double diviPorcent(double n1, double n2) {
+    	double resultado = n1 / ((n1 * n2) / 100);
     	return resultado;
     }
 	
-	public double multiPorcent(double n0, double n1) {
-    	double resultado = n0 * ((n0 * n1) / 100);
+	public double multiPorcent(double n1, double n2) {
+    	double resultado = n1 * ((n1 * n2) / 100);
     	return resultado;
     }
 	
-	public double subPorcent(double n0, double n1) {
-    	double resultado = n0 - ((n0 * n1) / 100);
+	public double subPorcent(double n1, double n2) {
+    	double resultado = n1 - ((n1 * n2) / 100);
     	return resultado;
     }
 	
-	public double adiPorcent(double n0, double n1) {
-    	double resultado = n0 + ((n0 * n1) / 100);
+	public double adiPorcent(double n1, double n2) {
+    	double resultado = n1 + ((n1 * n2) / 100);
     	return resultado;
     }
 	
-	public double multi(double n0, double n1) {
-    	double resultado = n0 * n1;
+	public double multi(double n1, double n2) {
+    	double resultado = n1 * n2;
     	return resultado;
     }
 	
-    public double sub(double n0, double n1) {
-    	double resultado = n0 - n1;
+	public double reciproco(double n1) {
+    	double resultado = 1 / n1;
+    	return resultado;
+    }
+	
+    public double sub(double n1, double n2) {
+    	double resultado = n1 - n2;
     	return resultado;
     }
     
-    public double adi(double n0, double n1) {
-    	double resultado = n0 + n1;
-    	return resultado;
-    }
-    
-    public double reciproco(double n0) {
-    	double resultado = 1/n0;
+    public double adi(double n1, double n2) {
+    	double resultado = n1 + n2;
     	return resultado;
     }
 }
